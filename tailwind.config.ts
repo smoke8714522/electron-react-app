a1af9bddf7ff1900c8bd6d0d208efa0749d5228c
guasam <guasamos@outlook.com>
2025-02-28 03:56:02 +0530

    Remove tailwind config

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: ['./app/index.html', './app/**/*.{js,jsx,ts,tsx}', './lib/**/*.{js,jsx,ts,tsx}'],
-  plugins: [],
-}
-
-export default config
