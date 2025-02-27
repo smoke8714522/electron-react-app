57f25cde664bacaa77fc831730ad3f0ea7c12705
guasam <guasamos@outlook.com>
2025-02-28 04:08:44 +0530

    Refactor App component and update styles for better layout

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -2,9 +2,5 @@ import WelcomeKit from '@/lib/welcome/WelcomeKit'
 import '../styles/app.css'
 
 export default function App() {
-  return (
-    <div>
-      <WelcomeKit />
-    </div>
-  )
+  return <WelcomeKit />
 }
